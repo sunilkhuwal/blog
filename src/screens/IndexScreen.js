68e098c4d061bd7b4b7af73b9e1ee17c52498c27
khuwals <sunil.khuwal@gmail.com>
2020-05-18 02:11:31 +0530

    Created new Project with details: 1. Used useContext 2. Used useState 3. Worked with the container to contain the App

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { Text, View, StyleSheet, FlatList, Button} from "react-native";
+import BlogContext from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const {data, addBlogPost} = useContext(BlogContext);
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button onPress={() => {addBlogPost()}} title='New Blog'/>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
