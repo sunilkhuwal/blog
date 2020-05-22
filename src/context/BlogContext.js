7b96b9e8920e69cd8e8c74feafaa33d19b6669f1
khuwals <sunil.khuwal@gmail.com>
2020-05-22 06:04:14 +0530

    redux in conjuction with Context for handling different context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,27 +1,20 @@
-import React,{useReducer} from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
-
-const blogReducer = (state , action) => {
+const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogpost':
-      return [...state, {title : `Blog post # ${state.length + 1}`}];
+    case "add_blogpost":
+      return [...state, { title: `Blog post # ${state.length + 1}` }];
     default:
       return state;
   }
-}
-
-export const BlogProvider = ({ children }) => {
-
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPost = () => {
-    dispatch({type: 'add_blogpost'});
-  }
-
-  return (
-    <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>{children}</BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+const addBlogPost = dispatch => {
+  return () => dispatch({ type: "add_blogpost" });
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
