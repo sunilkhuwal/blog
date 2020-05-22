8e7adcf7759168ca1fad44886552c4baf636211e
khuwals <sunil.khuwal@gmail.com>
2020-05-22 07:14:45 +0530

    delete blog post + new screen for showing individual blog

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import IndexScreen from "./src/screens/IndexScreen";
+import ShowScreen from "./src/screens/ShowScreen";
 import { Provider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
+    Show: ShowScreen,
   },
   {
     initialRouteName: "Index",
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,19 +2,31 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
-      return [...state, { title: `Blog post # ${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog post # ${state.length + 1}`,
+        },
+      ];
     default:
       return state;
   }
 };
 
-const addBlogPost = dispatch => {
+const addBlogPost = (dispatch) => {
   return () => dispatch({ type: "add_blogpost" });
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => dispatch({ type: "delete_blogpost", payload: id });
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
