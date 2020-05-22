8a7463241a193f10d5dfc311da75d585682bfa38
khuwals <sunil.khuwal@gmail.com>
2020-05-22 07:48:54 +0530

    show the actual data of screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useContext} from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
 
-const ShowScreen = ({navigation}) => {
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
+  );
+
   return (
     <View>
-
-  <Text>Show Screen - {navigation.id}</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   );
 };
