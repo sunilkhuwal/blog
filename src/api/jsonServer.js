f29feea616e5861005533a23318e1df7da8231b7
khuwals <sunil.khuwal@gmail.com>
2020-05-24 17:41:57 +0530

    Using the external server created locally using jsonserver and ngrok

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'http://f8f3bb9d.ngrok.io'
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,53 +1,72 @@
 import createDataContext from "./createDataContext";
+import jsonServer from "../api/jsonServer";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "get_blogposts":
+      return action.payload;
     case "edit_blogpost":
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
+    // case "add_blogpost":
+    //   return [
+    //     ...state,
+    //     {
+    //       id: Math.floor(Math.random() * 99999),
+    //       title: action.payload.title,
+    //       content: action.payload.content,
+    //     },
+    //   ];
     default:
       return state;
   }
 };
 
-const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    console.log("fefore calling api");
+    const response = await jsonServer.get("/blogposts");
+    console.log("got some response" + response.data);
     dispatch({
-      type: "add_blogpost",
-      payload: { title, content },
+      type: "get_blogposts",
+      payload: response.data,
     });
-    if(callback)
-      callback();
+  };
+};
+
+const addBlogPost = (dispatch) => {
+  return async (title, content, callback) => {
+    await jsonServer.post("/blogposts", { title, content });
+
+    // dispatch({
+    //   type: "add_blogpost",
+    //   payload: { title, content },
+    // });
+    if (callback) callback();
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
-    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
-    
-    if(callback)
-      callback();
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, {title,content});
+    // dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+
+    if (callback) callback();
   };
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => dispatch({ type: "delete_blogpost", payload: id });
+  return async (id) => {
+    const response = await jsonServer.delete(`/blogposts/${id}`);
+    dispatch({ type: "delete_blogpost", payload: id });
+  };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: "ExampleTitle", content: "work conent", id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
